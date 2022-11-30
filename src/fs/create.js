4bea73d3d2b5b5baee9630fc063e4c04d9b36acd
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-11-30 11:41:49 +0300

    feat: implement create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,30 @@
+import path from 'path';
+import { writeFile, access, constants } from 'fs/promises';
+
+const content = 'I am fresh and young';
+const errMsg = 'FS operation failed';
+const targetFolder = 'src/fs/files';
+const targetFileName = 'fresh.txt';
+const root = path.resolve();
+const targetPath = path.join(root, targetFolder, targetFileName);
+
 const create = async () => {
-    // Write your code here 
+  const isExist = await isFileExist(targetPath);
+
+  if (isExist) {
+    console.error(errMsg);
+  } else {
+    await writeFile(targetPath, content);
+  }
+};
+
+const isFileExist = async (path) => {
+  try {
+    await access(path, constants.F_OK);
+    return true;
+  } catch {
+    return false;
+  }
 };
 
 await create();
