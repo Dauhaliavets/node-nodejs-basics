2b90941c12ff8c82479d246363a1105bc222f331
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2022-12-06 02:00:45 +0300

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import path, { dirname } from 'path';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+import { fileURLToPath } from 'url';
+import { createHash } from 'node:crypto';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const targetFolder = 'files';
+const fileName = 'fileToCalculateHashFor.txt';
+const pathToFile = path.join(__dirname, targetFolder, fileName);
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+  const input = createReadStream(pathToFile);
+  input.pipe(hash).setEncoding('hex').pipe(stdout);
 };
 
 await calculateHash();
